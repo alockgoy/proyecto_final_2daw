1ac542a10b95fb8fc6c52c62007f269dd5425f2e
alockgoy <sanchezjerezalonso@gmail.com>
2025-05-10 10:09:43 +0200

    Añadido favicon a la vista de mi perfil

--- vistas/usuarios/my_profile.php
+++ vistas/usuarios/my_profile.php
@@ -131,11 +131,18 @@ if (isset($_POST["update_pic"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Enlace al css de bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHRKUc4W0kG879m7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHRKUc4W0kG879m7" crossorigin="anonymous" />
+
+    <!-- Enlace al archivo css -->
     <link href="../../css/my_profile.css" type="text/css" rel="stylesheet" />
     <title>Mi Perfil</title>
+
+    <!-- Para iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="shortcut icon" href="../../img/iconos_navegador/usuario.png" type="image/x-icon" />
 </head>
 
 <body>
