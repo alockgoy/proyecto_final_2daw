4e547f82817ecb4e01aa782788a60c794fe44a79
alockgoy <sanchezjerezalonso@gmail.com>
2025-05-10 10:06:00 +0200

    Añadido favicon a la vista login

--- vistas/usuarios/login.php
+++ vistas/usuarios/login.php
@@ -56,9 +56,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+    
     <link rel="stylesheet" type="text-css" href="../../css/login.css" />
     <title>Iniciar Sesión</title>
+
+    <!-- Para iconos -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="../../img/iconos_navegador/usuario.png" type="image/x-icon">
 </head>
 
 <body>
