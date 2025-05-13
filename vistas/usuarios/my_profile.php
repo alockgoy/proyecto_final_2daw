28603d03954df9b354b895578f61ec0175a5a57a
alockgoy2 <sanchezjerezalonso@gmail.com>
2025-05-13 18:22:46 +0200

    Desconozco qué hacía lo que he borrado

--- vistas/usuarios/my_profile.php
+++ vistas/usuarios/my_profile.php
@@ -328,9 +328,7 @@ if (isset($_POST["update_pic"])) {
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Enlace al Javascript de mi perfil -->
     <script src="../../js/my_profile.js"></script>
